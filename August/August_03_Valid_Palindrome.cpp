3bc6a2cf7d4a89facb12ce74ef29a93e88726d1b
Priyanshi Sharma <priyanshi417@gmail.com>
2020-08-04 14:05:35 +0530

    Day 3

---
+++ August/August_03_Valid_Palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+       int beg=0, end=s.size()-1;
+        while(beg<end)
+		{
+		
+            if(!isalnum(s[beg]))
+                beg++;
+				
+            if(!isalnum(s[end]))
+                end--;
+				
+            if(isalnum(s[beg]) && isalnum(s[end]))
+			{
+                if(tolower(s[beg++]) != tolower(s[end--]))
+                return false;
+            }
+        }
+        return true;
+    }
+};
