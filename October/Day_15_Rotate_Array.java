95b4e371dfe1084a9bd3ea2e0673f75858cf536f
techcodno <72292911+techcodno@users.noreply.github.com>
2020-10-17 08:19:57 +0530

    Create Day_15_Rotate_Array.java

---
+++ October/Day_15_Rotate_Array.java
@@ -0,0 +1,23 @@
+
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        int offset = k % nums.length;
+        int count = 0;
+        for (int start = 0; count < nums.length; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + offset) % nums.length;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                count++;
+            } while (start != current);
+        }
+    }
+}
