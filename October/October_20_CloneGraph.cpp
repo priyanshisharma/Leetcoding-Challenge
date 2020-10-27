e7467fb72df5a85946ec7eb7f5a3eee7d1bcdea2
Priyanshi Sharma <priyanshi417@gmail.com>
2020-10-27 10:34:33 +0530

    Update October_20_CloneGraph.cpp

--- October/October_20_CloneGraph.cpp
+++ October/October_20_CloneGraph.cpp
@@ -1,3 +1,26 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
 class Solution {
 public:
     //store the nodes
